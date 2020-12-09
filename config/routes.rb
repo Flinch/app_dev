836629f705780a4ed3f2d4e272867a0d9abf13c0
Flinch <Abdulmalikibrahim072@gmail.com>
2020-12-08 20:27:15 -0500

    front end user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#start"
   get 'about', to: "pages#about"
-
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
 end
