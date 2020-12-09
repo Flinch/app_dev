d44ae3cc1b2f7424161dff782cd4e9c1c4bf1064
Flinch <Abdulmalikibrahim072@gmail.com>
2020-12-08 22:41:27 -0500

    added signup functionality to back-end, active link on home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_param)
+		if @user.save
+			flash[:notice] = "Welcome to the site #{@user.username}."
+			redirect_to articles_path
+		else
+			render "new"
+		end
+	end 
+
+	private
+
+		def set_user
+			@user = User.find(params[:id]) 
+		end
+
+		def user_param
+			params.require(:user).permit(:username, :email, :password)
+		end
+
+
 end
