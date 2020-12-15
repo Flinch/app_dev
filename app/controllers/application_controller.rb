7e05a0d8a76a051257ff1502ff3ec0f38b037a48
Flinch <Abdulmalikibrahim072@gmail.com>
2020-12-14 21:49:15 -0500

    layout changes including: color, buttons, forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+	helper_method :current_user, :is_logged_in
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def is_logged_in
+		!!current_user
+	end
+
+	def require_user
+		if !is_logged_in
+			flash[:alert] = "You need to be signed in to perform this action"
+			redirect_to login_path
+		end
+	end
+
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update]
 
 	def show
 	end
@@ -14,10 +16,10 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_param)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:notice] = "Article was created successfully."
-			redirect_to @article
+			redirect_to current_user
 		else
 			render "new"
 		end
@@ -51,4 +53,12 @@ class ArticlesController < ApplicationController
 		def article_param
 			params.require(:article).permit(:title, :description)
 		end
+
+		def require_same_user
+			if current_user != @article.user
+				flash[:alert] = "You need to be logged in to your own profile to perform this action"
+				redirect_to @article
+			end
+		end
+
 end
