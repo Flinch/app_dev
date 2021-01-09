bfa8ce2ab9b16c7baf7ec26544dac195efb20837
Flinch <Abdulmalikibrahim072@gmail.com>
2021-01-09 17:20:56 -0500

    css changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-	 validates :description, presence: true, length: { minimum: 1, maximum: 300 }
+	 validates :description, presence: true, length: { minimum: 1, maximum: 1000 }
 	 belongs_to :user
   	 belongs_to :article
 end
